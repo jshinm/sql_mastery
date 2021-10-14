ba54ba1a1ed0fc07bac134c66bcd7d241abd1d2d
Jong Shin <jshin.m@gmail.com>
2021-10-14 17:31:42 -0400

    basic query

--- leetcode_challenges/20211014_big_countries.sql
+++ leetcode_challenges/20211014_big_countries.sql
@@ -23,4 +23,8 @@
 -- +--------------+-------------+--------------+
 
 -- # Write your MySQL query statement below
-
+select name, population, area 
+from World
+where
+area > 3000000 or
+population > 25000000
