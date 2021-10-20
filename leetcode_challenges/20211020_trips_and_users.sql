00d01ea629ab546f045fc85160d7f473ee3fa0db
Jong Shin <jshin.m@gmail.com>
2021-10-20 03:04:46 -0400

    add shorter queries version

--- leetcode_challenges/20211020_trips_and_users.sql
+++ leetcode_challenges/20211020_trips_and_users.sql
@@ -149,3 +149,14 @@ from
     on t1.Request_at = t2.Request_at
 
 where t1.Request_at between '2013-10-01' and '2013-10-03'
+
+
+
+-- shorter version by sachinjha
+select t.Request_at Day,
+       ROUND((count(IF(t.status!='completed',TRUE,null))/count(*)),2) as 'Cancellation Rate'
+from Trips t where 
+t.Client_Id in (Select Users_Id from Users where Banned='No') 
+and t.Driver_Id in (Select Users_Id from Users where Banned='No')
+and t.Request_at between '2013-10-01' and '2013-10-03'
+group by t.Request_at;
