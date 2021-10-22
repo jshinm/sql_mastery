4a5515b8efca401365bb9c591a58c145539db784
Jong Shin <jshin.m@gmail.com>
2021-10-22 09:58:26 -0400

    string pattern matching

--- hacker_rank/20211021_weather observation.sql
+++ hacker_rank/20211021_weather observation.sql
@@ -23,3 +23,11 @@ limit 1;
 select city, length(city) as len from station
 order by len desc, city asc
 limit 1;
+
+-- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+select distinct city from station
+where city like 'a%' or
+city like 'e%' or
+city like 'i%' or
+city like 'o%' or
+city like 'u%'
