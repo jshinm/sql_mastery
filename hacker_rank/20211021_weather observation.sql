04eb8221a2f3d825dfe898bfdb460eefed5e93b5
Jong Shin <jshin.m@gmail.com>
2021-10-22 07:50:46 -0400

    subtract two aggs from one table

--- hacker_rank/20211021_weather observation.sql
+++ hacker_rank/20211021_weather observation.sql
@@ -3,3 +3,8 @@
 
 select distinct city from station
 where id % 2 = 0
+
+-- Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+-- For example, if there are three records in the table with CITY values 'New York', 'New York', 'Bengalaru', 
+-- there are 2 different city names: 'New York' and 'Bengalaru'. The query returns , because
+select count(s1.city) - count(distinct s1.city) as cnt from station as s1
