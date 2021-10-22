d9c1880e2891b5161d00640bb847bf2cc73ff0ca
Jong Shin <jshin.m@gmail.com>
2021-10-22 09:47:17 -0400

    double order by

--- hacker_rank/20211021_weather observation.sql
+++ hacker_rank/20211021_weather observation.sql
@@ -8,3 +8,18 @@ where id % 2 = 0
 -- For example, if there are three records in the table with CITY values 'New York', 'New York', 'Bengalaru', 
 -- there are 2 different city names: 'New York' and 'Bengalaru'. The query returns , because
 select count(s1.city) - count(distinct s1.city) as cnt from station as s1
+
+-- Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). 
+-- If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+-- When ordered alphabetically, the CITY names are listed as ABC, DEF, PQRS, and WXY, with lengths and
+-- The longest name is PQRS, but there are  options for shortest named city. Choose ABC, because it comes first alphabetically.
+
+-- Note: You can write two separate queries to get the desired output. It need not be a single query.
+
+select city, length(city) as len from station
+order by len asc, city asc
+limit 1;
+
+select city, length(city) as len from station
+order by len desc, city asc
+limit 1;
