c61dbd44dc3e27c96cd69ceb029e2385d863f9a9
Jong Shin <jshin.m@gmail.com>
2021-10-23 00:02:07 -0400

    regexp two where statements

--- hacker_rank/20211021_weather observation.sql
+++ hacker_rank/20211021_weather observation.sql
@@ -37,3 +37,9 @@ city like 'u%'
 
 select city from station
 where city regexp '^[aeiou].*[aeiou]$'
+
+-- Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. 
+-- Your result cannot contain duplicates.
+select distinct city from station
+where city regexp '^[^aeiou].*' or
+city regexp '.*[^aeiou]$'
