19ced1a1f8545b6235eb9e9c7c7140374f80e257
Jong Shin <jshin.m@gmail.com>
2021-10-22 23:53:51 -0400

    regex method

--- hacker_rank/20211021_weather observation.sql
+++ hacker_rank/20211021_weather observation.sql
@@ -31,3 +31,9 @@ city like 'e%' or
 city like 'i%' or
 city like 'o%' or
 city like 'u%'
+
+-- Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. 
+-- Your result cannot contain duplicates.
+
+select city from station
+where city regexp '^[aeiou].*[aeiou]$'
