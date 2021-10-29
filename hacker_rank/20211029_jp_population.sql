4d043a24d9ff51fe186fb59227731a42a972dee5
Jong Shin <jshin.m@gmail.com>
2021-10-29 04:57:57 -0400

    cast string into float

--- hacker_rank/20211029_jp_population.sql
+++ hacker_rank/20211029_jp_population.sql
@@ -2,3 +2,16 @@
 
 select sum(population) from city
 where countrycode = 'JPN'
+
+-- Query the difference between the maximum and minimum populations in CITY.
+select max(population)-min(population) from city
+
+-- Samantha was tasked with calculating the average monthly salaries for all employees in the EMPLOYEES table, 
+-- but did not realize her keyboard's 0 key was broken until after completing the calculation. 
+-- She wants your help finding the difference between her miscalculation (using salaries with any zeros removed), 
+-- and the actual average salary.
+
+-- Write a query calculating the amount of error (i.e.:  average monthly salaries), and round it up to the next integer.
+
+-- round up the answer before changing to integer
+select cast(ceiling(avg(cast(salary as float)) - avg(cast(replace(salary, 0, '') as float))) as int) from employees
