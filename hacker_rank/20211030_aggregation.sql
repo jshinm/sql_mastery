64c76055d404d824ccbc7fc54b6f63f16ecc45cd
Jong Shin <jshin.m@gmail.com>
2021-10-31 08:04:28 -0400

    more agg solutions

--- hacker_rank/20211030_aggregation.sql
+++ hacker_rank/20211030_aggregation.sql
@@ -16,10 +16,21 @@ with tmp as (
 select earnings, count(rnk) from tmp
 group by earnings
 
-
 -- Query the following two values from the STATION table:
 
--- The sum of all values in LAT_N rounded to a scale of  decimal places.
--- The sum of all values in LONG_W rounded to a scale of  decimal places.
-
+-- The sum of all values in LAT_N rounded to a scale of 2 decimal places.
+-- The sum of all values in LONG_W rounded to a scale of 2 decimal places.
 select cast(sum(lat_n) as decimal(38,2)) lat, cast(sum(long_w) as decimal(38,2)) lon from station
+
+-- Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than 137.2345. 
+-- Truncate your answer to 4 decimal places.
+select format(max(lat_n), '#.0000') from station
+where lat_n < 137.2345
+
+-- Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that 
+-- is less than 137.2345. Round your answer to 4 decimal places.
+select format(long_w, '#.0000') from (
+select long_w, row_number() over (order by lat_n desc) rn from station
+where lat_n < 137.2345
+) tmp
+where rn = 1
